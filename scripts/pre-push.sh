2c30719bf330b823cd9274aa61270e6cb9717943
Mohammadreza Varasteh <58051486+The-Daishogun@users.noreply.github.com>
2023-05-08 19:58:48 +0330

    skip commiting if remote is github (#2)

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $(git config --get remote.origin.url) == *"github"* ]]; then
+  echo "skipping sync commit push since remote is github."
+  exit 0
+fi
+
 syncommit push
 
 exit 0
